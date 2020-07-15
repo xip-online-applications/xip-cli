7ce3430e0cb442a37d53ed83d20da609dd93c9bd
Youri Lefers <youri.lefers@x-ip.nl>
2020-07-15 13:31:05 +0200

    Cleaned windows install script

--- install/windows.ps1
+++ install/windows.ps1
@@ -1,21 +1,18 @@
 function InstallXipCli {
-    &{
-        # The install dir
-        $installDir = 'C:\Program Files\XIP\bin'
+    # The install dir
+    $installDir = 'C:\Program Files\XIP\bin'
 
-        # Create install dir if it doesn't exist
-        New-Item -Path $installDir -Force -ItemType Directory
+    # Create install dir if it doesn't exist
+    New-Item -Path $installDir -Force -ItemType Directory
 
-        # Download the file
-        Invoke-WebRequest 'https://github.com/xip-online-applications/xip-cli/releases/latest/download/x-ip_windows_amd64.exe' -Outfile "$installDir\x-ip.exe"
+    # Download the file
+    Invoke-WebRequest 'https://github.com/xip-online-applications/xip-cli/releases/latest/download/x-ip_windows_amd64.exe' -Outfile "$installDir\x-ip.exe"
 
-        # Update Path environment variable
-        $regexAddPath = [regex]::Escape($installDir)
-        $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch "^$regexAddPath\\?"}
-        $env:Path = ($arrPath + $installDir) -join ';'
+    # Update Path environment variable
+    $regexAddPath = [regex]::Escape($installDir)
+    $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch "^$regexAddPath\\?"}
+    $env:Path = ($arrPath + $installDir) -join ';'
 
-        # Write new path env variable
-        setx PATH $env:Path -m
-#         [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-    } 6> $null
+    # Write new path env variable
+    setx PATH $env:Path -m
 } InstallXipCli
